72b3d9c9ebb3968ccff1c19289bd725da6e3c5b6
Alexander Ivanov - Karamfil <karamfil.pc@gmail.com>
2013-09-14 00:54:55 +0300

    angular js router, fridge functionality

--- scripts/livescript/interface.ls
+++ scripts/livescript/interface.ls
@@ -1,17 +1,50 @@
-$ !->
-	# $tab_cnts = $ \.tab-cnt
-	# $ '.tabs a' .click ->
-	# 	$this = $(@)
-	# 	$tab_cnts.removeClass \selected .filter $this.attr \href .addClass \selected
-	# 	$this.parent!addClass \selected .siblings!removeClass \selected
+l = console.log
+
+NAVIGATION = [
+	* title: 'Home'			href : '/'
+	* title: 'Cookbook'		href : '/cookbook'
+	* title: 'Fridge'		href : '/fridge'
+]
+
+app = angular.module \Kitchen, [\ngRoute, \ui.select2]
+
+app.config [\$routeProvider, \$locationProvider, !($routeProvider, $locationProvider)->
+	$routeProvider
+		..when \/,						{ templateUrl: \/templates/home.html,			controller: \HomeCtrl }
+		..when \/cookbook,				{ templateUrl: \/templates/cookbook.html,		controller: \CookbookCtrl }
+		..when \/fridge,				{ templateUrl: \/templates/ingredients.html,	controller: \IngredientsCtrl }
+		..otherwise 					{ templateUrl: \/templates/error.html }
+	
+	$locationProvider.html5Mode true
+]
+
+app.controller \BaseCtrl, [\$scope, \$http, \$route, !($scope, $http)->
+	do $scope.set_title = !(title = '', heading = null)->
+		$scope.title = "#title | Kitchen"
+		$scope.heading = heading ? title
+]
+
+app.controller \NavigationCtrl, [\$scope, \$location, !($scope, $location)->
+	$scope.items = NAVIGATION
+	$scope.is_active = (loc)-> if loc is \/ + $location.path!split \/ .1 then \active else ''
+]
+
+
+app.controller \HomeCtrl, [\$scope, \$http, !($scope, $http)->
+	$scope.set_title \Home 'Virtual Kitchen'
+]
+
+app.controller \CookbookCtrl, [\$scope, \$http, !($scope, $http)->
+	$scope.set_title \Cookbook
+]
+
+app.controller \IngredientsCtrl, [\$scope, \$http, !($scope, $http)->
+	$scope.set_title \Fridge
+	
+	$http.get \/data/ingredients-cats.json? .success !(data)->
+		$scope.categories = data
+	
+	$http.get \/data/ingredients.json? .success !(data)->
 		
-	# 	false
-
-
-
-Details_Controller = !($scope)-> $scope.data = DATA.details
-Education_Controller = !($scope)-> $scope.data = DATA.education
-Events_Controller = !($scope)-> $scope.data = DATA.events
-Skills_Controller = !($scope)-> $scope.data = DATA.skills
-Freelance_Controller = !($scope)-> $scope.data = DATA.freelance
-Experience_Controller = !($scope)-> $scope.data = DATA.experience
+		$scope.items = data
+]
--- scripts/livescript/livescript/interface.js
+++ scripts/livescript/livescript/interface.js
@@ -1,20 +1,80 @@
-var Details_Controller, Education_Controller, Events_Controller, Skills_Controller, Freelance_Controller, Experience_Controller;
-$(function(){});
-Details_Controller = function($scope){
-  $scope.data = DATA.details;
-};
-Education_Controller = function($scope){
-  $scope.data = DATA.education;
-};
-Events_Controller = function($scope){
-  $scope.data = DATA.events;
-};
-Skills_Controller = function($scope){
-  $scope.data = DATA.skills;
-};
-Freelance_Controller = function($scope){
-  $scope.data = DATA.freelance;
-};
-Experience_Controller = function($scope){
-  $scope.data = DATA.experience;
-};
+var l, NAVIGATION, app;
+l = console.log;
+NAVIGATION = [
+  {
+    title: 'Home',
+    href: '/'
+  }, {
+    title: 'Cookbook',
+    href: '/cookbook'
+  }, {
+    title: 'Fridge',
+    href: '/fridge'
+  }
+];
+app = angular.module('Kitchen', ['ngRoute', 'ui.select2']);
+app.config([
+  '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    var x$;
+    x$ = $routeProvider;
+    x$.when('/', {
+      templateUrl: '/templates/home.html',
+      controller: 'HomeCtrl'
+    });
+    x$.when('/cookbook', {
+      templateUrl: '/templates/cookbook.html',
+      controller: 'CookbookCtrl'
+    });
+    x$.when('/fridge', {
+      templateUrl: '/templates/ingredients.html',
+      controller: 'IngredientsCtrl'
+    });
+    x$.otherwise({
+      templateUrl: '/templates/error.html'
+    });
+    $locationProvider.html5Mode(true);
+  }
+]);
+app.controller('BaseCtrl', [
+  '$scope', '$http', '$route', function($scope, $http){
+    ($scope.set_title = function(title, heading){
+      title == null && (title = '');
+      heading == null && (heading = null);
+      $scope.title = title + " | Kitchen";
+      $scope.heading = heading != null ? heading : title;
+    })();
+  }
+]);
+app.controller('NavigationCtrl', [
+  '$scope', '$location', function($scope, $location){
+    $scope.items = NAVIGATION;
+    $scope.is_active = function(loc){
+      if (loc === '/' + $location.path().split('/')[1]) {
+        return 'active';
+      } else {
+        return '';
+      }
+    };
+  }
+]);
+app.controller('HomeCtrl', [
+  '$scope', '$http', function($scope, $http){
+    $scope.set_title('Home', 'Virtual Kitchen');
+  }
+]);
+app.controller('CookbookCtrl', [
+  '$scope', '$http', function($scope, $http){
+    $scope.set_title('Cookbook');
+  }
+]);
+app.controller('IngredientsCtrl', [
+  '$scope', '$http', function($scope, $http){
+    $scope.set_title('Fridge');
+    $http.get('/data/ingredients-cats.json?').success(function(data){
+      $scope.categories = data;
+    });
+    $http.get('/data/ingredients.json?').success(function(data){
+      $scope.items = data;
+    });
+  }
+]);
